6de21fa4826778022a560db085dac9bd0e24dab2
patoledavid <patoledavid@gmail.com>
2024-04-01 18:45:16 +1100

    first commit

---
+++ Dto/ReviewerDto.cs
@@ -0,0 +1,10 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Dto;
+
+public class ReviewerDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
